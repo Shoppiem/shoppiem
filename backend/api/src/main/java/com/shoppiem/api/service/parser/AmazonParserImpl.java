934359aa325407741f9b656a21dc925993065aaa
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-05-31 11:22:31 -0500

    Implement product page parser

--- backend/api/src/main/java/com/shoppiem/api/service/parser/AmazonParserImpl.java
+++ backend/api/src/main/java/com/shoppiem/api/service/parser/AmazonParserImpl.java
@@ -1,8 +1,20 @@
 package com.shoppiem.api.service.parser;
 
+import io.netty.util.internal.StringUtil;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jgit.util.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 /**
  * @author Biz Melesse created on 5/29/23
@@ -14,6 +26,121 @@ public class AmazonParserImpl implements AmazonParser {
 
   @Override
   public void processSoup(String sku, String soup) {
+    Document doc = Jsoup.parse(soup);
+    String titleXPath = "//*[@id=\"productTitle\"]";
+    String sellerXPath = "//*[@id=\"bylineInfo\"]";
+    String ratingXPath = "//*[@id=\"acrPopover\"]/span[1]/a";
+    String reviewCountXPath = "//*[@id=\"acrCustomerReviewText\"]";
+    String featuresXPath = "//*[@id=\"feature-bullets\"]/ul/li[1]/span";
+    String overviewFeatureXPath = "//*[@id=\"productOverview_feature_div\"]/div/table/tbody";
+    String productDescriptionXPath = "//*[@id=\"productDescription\"]/p[1]/span";
+    String imageXPath = "//*[@id=\"landingImage\"]";
+    String imageUrl = getImage(doc, imageXPath);
+    Double rating = getRating(doc, ratingXPath);
+    String title = getTitle(doc, titleXPath);
+    String seller = getSeller(doc, sellerXPath);
+    Integer numReviews = getNumReviews(doc, reviewCountXPath);
+
+    List<String> features = new ArrayList<>();
+    walkHelper(doc, featuresXPath, features);
+
+
+    List<String> overviewTableData = new ArrayList<>();
+    walkHelper(doc, overviewFeatureXPath, overviewTableData);
+    overviewTableData = mapTableColumns(overviewTableData);
+
+    List<String> productDescription = new ArrayList<>();
+    walkHelper(doc, productDescriptionXPath, productDescription);
+
+    
+
+    String bookDescFeatureXPath = "//*[@id=\"bookDescription_feature_div\"]/div/div[1]/p[1]/span[4]";
+
+
+
+
+    String productDescriptionXPathType2 = "//*[@id=\"aplus\"]/div/div[1]/div/p[1]";
+
 
   }
+
+  private String getImage(Document doc, String imageXPath) {
+    for (Attribute attribute : doc.selectXpath(imageXPath).get(0).attributes()) {
+      if (attribute.getKey().equals("src")) {
+        return attribute.getValue();
+      }
+    }
+     return "";
+  }
+
+  private Integer getNumReviews(Document doc, String reviewCountXPath) {
+    return Integer.parseInt(doc.selectXpath(reviewCountXPath)
+        .get(0)
+        .childNodes().get(0)
+        .toString().replace(" ratings", ""));
+  }
+
+  private String getSeller(Document doc, String sellerXPath) {
+    return StringUtils.capitalize(
+        doc.selectXpath(sellerXPath).get(0).childNodes().get(0).toString().replace("Visit ", ""));
+  }
+
+  private String getTitle(Document doc, String titleXPath) {
+    return doc.selectXpath(titleXPath).get(0).childNodes().get(0).toString();
+  }
+
+  private Double getRating(Document doc, String ratingXPath) {
+    String ratingClass = "a-size-base a-color-base";
+    double rating = 0.0;
+    for (Element element : doc.selectXpath(ratingXPath)) {
+      for (Node childNode : element.childNodes()) {
+        if (childNode.attr("class").equals(ratingClass)) {
+          rating = Double.parseDouble(childNode.childNodes().get(0).toString());
+        }
+      }
+    }
+    return rating;
+  }
+
+  private void walkHelper(Document doc, String xPath, List<String> ret) {
+    for (Element element : doc.selectXpath(xPath)) {
+      walk(element, ret);
+    }
+  }
+
+  private List<String> mapTableColumns(List<String> tableData) {
+    // Remap table columns into key-value pairs
+    // Ideally, there should be an equal number of keys and values
+    String key = "";
+    List<String> map = new ArrayList<>();
+    for (int i = 0; i < tableData.size(); i++) {
+      if (i % 2 == 0) {
+        key = tableData.get(i);
+      } else {
+        map.add(key + ": " + tableData.get(i));
+        key = "";
+      }
+    }
+    return map;
+  }
+
+  private void walk(Node root, List<String> allText) {
+    if (root != null) {
+      if (root instanceof TextNode) {
+        String text = ((TextNode) root)
+            .text()
+            .replace("See more", "")
+            .trim()
+            .strip();
+        if (!ObjectUtils.isEmpty(text)) {
+          allText.add(text);
+        }
+      } else if (root instanceof  Element) {
+        for (Node childNode : ((Element) root).childNodes()) {
+          walk(childNode, allText);
+        }
+      }
+    }
+  }
+
 }
--- backend/api/src/test/java/com/shoppiem/api/service/scraper/ScraperServiceIntegrationTest.java
+++ backend/api/src/test/java/com/shoppiem/api/service/scraper/ScraperServiceIntegrationTest.java
@@ -1,20 +1,18 @@
 package com.shoppiem.api.service.scraper;
 
-
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertNotNull;
-import static org.testng.AssertJUnit.assertTrue;
-
-import com.shoppiem.api.ProductCreateResponse;
-import com.shoppiem.api.ProductRequest;
 import com.shoppiem.api.service.ServiceTestConfiguration;
-import com.shoppiem.api.service.product.ProductService;
+import com.shoppiem.api.service.parser.AmazonParser;
 import com.shoppiem.api.utils.migration.FlywayMigration;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.internal.util.FileCopyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -32,6 +30,9 @@ public class ScraperServiceIntegrationTest extends AbstractTestNGSpringContextTe
 
     @Autowired
     private ScraperService scraperService;
+
+    @Autowired
+    private AmazonParser amazonParser;
 
     @Autowired
     private FlywayMigration flywayMigration;
@@ -61,5 +62,20 @@ public class ScraperServiceIntegrationTest extends AbstractTestNGSpringContextTe
         String url = "https://www.amazon.com/Belkin-Boost%E2%86%91ChargeTM-Wireless-Compatible-Kickstand/dp/B0BXRMCC31/?_encoding=UTF8&pd_rd_w=dyu4M&content-id=amzn1.sym.c1df8aef-5b8d-403a-bbaa-0d55ea81081f&pf_rd_p=c1df8aef-5b8d-403a-bbaa-0d55ea81081f&pf_rd_r=CVH3RSQQQDGMZPB008AQ&pd_rd_wg=B4Bru&pd_rd_r=08f1c561-28fd-45c0-8d24-ca21537303c7&ref_=pd_gw_gcx_gw_EGG-Graduation-23-1a&th=1";
         String sku = "B0BXRMCC31";
         scraperService.getContent(sku, url);
+    }
+
+    @Test
+    public void amazonAccessoryProductPageParserTest() {
+        String sku = "B0BXRMCC31";
+        String soup = loadFromFile("scraper/amazonProductPage_Accessories.html");
+        amazonParser.processSoup(sku, soup);
+    }
+
+    @SneakyThrows
+    private String loadFromFile(String path) {
+        InputStream resource = new ClassPathResource(path).getInputStream();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource)) ) {
+            return FileCopyUtils.copyToString(reader);
+        }
     }
 }
