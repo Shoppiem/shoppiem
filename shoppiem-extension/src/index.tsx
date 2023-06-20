db948af281ef95fd07e1af4eff5ae7ece08ba09d
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-06-19 22:21:23 -0500

    Render floating button on Amazon product page only

--- shoppiem-extension/src/index.tsx
+++ shoppiem-extension/src/index.tsx
@@ -5,32 +5,36 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 async function init() {
-  // Create div wrapper
-  const body = document.body;
-  const bodyWrapper = document.createElement('div');
-  bodyWrapper.id = 'original-body-wrapper';
-  bodyWrapper.className = 'h-full w-full overflow-auto relative ease-in-out duration-300';
+  const href = window.location.href
+  if (href.includes("amazon.com") && href.includes("/dp/")) {
+    // Create div wrapper
+    const body = document.body;
+    const bodyWrapper = document.createElement('div');
+    bodyWrapper.id = 'original-body-wrapper';
+    bodyWrapper.className = 'h-full w-full overflow-auto relative ease-in-out duration-300';
 
-  // Move the body's children into this wrapper
-  while (body.firstChild) {
-    bodyWrapper.appendChild(body.firstChild);
+    // Move the body's children into this wrapper
+    while (body.firstChild) {
+      bodyWrapper.appendChild(body.firstChild);
+    }
+
+    bodyWrapper.style.overflow = 'auto';
+    bodyWrapper.style.height = '100vh';
+
+    // Append the wrapper to the body
+    body.style.overflow = 'hidden';
+    body.style.margin = '0';
+    body.appendChild(bodyWrapper);
+
+    // create react app
+    const app = document.createElement('div');
+    app.id = 'shoppiem-root';
+
+    body.appendChild(app);
+    const root = createRoot(app!);
+    root.render(<App/>);
   }
-
-  bodyWrapper.style.overflow = 'auto';
-  bodyWrapper.style.height = '100vh';
-
-  // Append the wrapper to the body
-  body.style.overflow = 'hidden';
-  body.style.margin = '0';
-  body.appendChild(bodyWrapper);
-
-  // create react app
-  const app = document.createElement('div');
-  app.id = 'shoppiem-root';
-
-  body.appendChild(app);
-  const root = createRoot(app!);
-  root.render(<App />);
 }
 
-init();
+init()
+.catch(e => console.log(e));
