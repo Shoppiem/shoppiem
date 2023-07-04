fdb3cb10e880a723dc567a93d84df2319c897267
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-07-03 19:52:52 -0500

    Fix build failure

--- frontend/components/Testimonials/SingleTestimonial.tsx
+++ frontend/components/Testimonials/SingleTestimonial.tsx
@@ -7,7 +7,7 @@ const starIcon = (
 );
 
 const SingleTestimonial = ({ testimonial }: { testimonial: Testimonial }) => {
-  const { star, name, image, content, designation } = testimonial;
+  const { star, name, content, designation } = testimonial;
 
   let ratingIcons = [];
   for (let index = 0; index < star; index++) {
